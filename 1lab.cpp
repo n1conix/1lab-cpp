ec6ff251834cd1b8a7f9b9c7c1804613d67691bc
NicoN1x <vadimnedrica@gmail.com>
2025-09-16 09:24:01 +0300

    Smenil cod polnostiu

--- 1lab.cpp
+++ 1lab.cpp
@@ -1,41 +1,46 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
+
 using namespace std;
 
 int main() {
     double a, b, c;
     double x_start, x_end, dx;
 
+    // Ввод данных
     cout << "Введите a, b, c: ";
     cin >> a >> b >> c;
-
-    cout << "Введите начало интервала xн, конец xк и шаг dx: ";
+    cout << "Введите Xнач, Xкон и dX: ";
     cin >> x_start >> x_end >> dx;
+
+    // Заголовок таблицы
+    cout << setw(10) << "X" << setw(20) << "F(X)" << endl;
+    cout << "-----------------------------------" << endl;
 
     for (double x = x_start; x <= x_end; x += dx) {
         double F;
+        int Ac = static_cast<int>(a);
+        int Bc = static_cast<int>(b);
+        int Cc = static_cast<int>(c);
 
-        if (c + b < 0 && a != 0) {
-            F = a * pow(cos(x), 2) + c;
-        } 
-        else if (c + b > 0 && a == 0) {
-            if (a * x - log(b * x) != 0) // защита от деления на 0
-                F = (pow(b, 2) - x) / (a * x - log(b * x));
-            else {
-                cout << "Деление на 0 при x=" << x << endl;
-                continue;
-            }
-        } 
-        else {
-            if (3 * sin(x) + 4 * log(2 * x) != 0)
-                F = (pow(b, 2) - c * x) / (3 * sin(x) + 4 * log(2 * x));
-            else {
-                cout << "Деление на 0 при x=" << x << endl;
-                continue;
-            }
+        bool conditionBitwise = ((Ac | Bc) & Cc) != 0;
+
+        // Определяем F в зависимости от условий
+        if (x < 0.6 && (b + c != 0)) {
+            F = a * x * x + b * b + c;
+        } else if (x > 0.6 && (b + c == 0)) {
+            F = a + b + c;
+        } else {
+            F = a * b + c;
         }
 
-        cout << "x = " << x << "  F(x) = " << F << endl;
+        // Если побитовое условие не выполнено, выводим как целое
+        if (!conditionBitwise) {
+            cout << setw(10) << x << setw(20) << static_cast<int>(F) << endl;
+        } else {
+            cout << setw(10) << x << setw(20) << fixed << setprecision(4) << F << endl;
+        }
     }
 
     return 0;
